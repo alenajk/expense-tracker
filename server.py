e7f0bb7efecf3c132d58e9164b1102fe569c5469
alenajk <alenak@gmail.com>
2016-06-15 16:05:12 -0400

    added new route for incoming messages

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from twilio.rest import TwilioRestClient
 import twilio.twiml
 app = Flask(__name__)
@@ -18,9 +18,17 @@ def hello():
     msgs = [str(msg.body) for msg in client.messages.list()]
     print msgs, type(msgs), msgs[1]
     print dir(client.messages)
-    # import pdb
-    # pdb.set_trace()
     return render_template('homepage.html', msgs=msgs)
+
+@app.route("/newmsg", methods=['GET','POST'])
+def savemsg():
+	print "testing"
+	if request.method == "POST":
+		msg = request.form.get('Body')
+		print 'request.body :', request.body
+		print 'msg :', msg
+
+	return 'Testing newmsg app route'
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
