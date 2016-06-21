1eab334417999dcecb712aa403935de7adc5ff45
alenajk <alenak@gmail.com>
2016-06-21 18:09:00 -0400

    Added sqlalchemy

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from twilio.rest import TwilioRestClient
 import twilio.twiml
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+db = SQLAlchemy(app)
 
 # Source twilio credentials
 account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
@@ -13,16 +18,24 @@ if not account_sid:
 
 client = TwilioRestClient(account_sid, auth_token)
 
+class Expense(db.Model):
+	expenseid = db.Column(db.Integer, primary_key=True)
+	date = db.Column(db.Date)
+	amount = db.Column(db.Float)
+	category = db.Column(db.String(80))
+	description = db.Column(db.String(80))
+
 @app.route("/")
 def hello():
-    msgs = [str(msg.body) for msg in client.messages.list()]
-    print msgs, type(msgs), msgs[1]
-    print dir(client.messages)
-    return render_template('homepage.html', msgs=msgs)
+
+	print db.session.query(Expense).all()
+	msgs = [str(msg.body) for msg in client.messages.list()]
+
+	return render_template('homepage.html', msgs=msgs)
 
 @app.route("/newmsg", methods=['GET','POST'])
 def savemsg():
-	print "testing"
+
 	if request.method == "POST":
 		msg = request.form.get('Body')
 		print 'msg :', msg
