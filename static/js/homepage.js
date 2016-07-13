2cb6455c6a6bc38aa2b4db3aeb16f14fa4037fcf
alenajk <alenak@gmail.com>
2016-07-13 16:25:08 -0400

    Added total and javascript to update total depending on categories checked

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,10 +1,28 @@
 $('.checkbox').click(function(){
     var cat = $(this).attr('value');
     if ($(this).attr('checked')){
-        $('.'+cat).hide()
+        // $('.'+cat).hide()
+        $('.'+cat).addClass('hidden');
         $(this).attr('checked',false)
     } else {
         $(this).attr('checked',true)
-        $('.'+cat).show()
+        // $('.'+cat).show()
+        $('.'+cat).removeClass('hidden');
     };
+    get_total();
+});
+
+function get_total() {
+    var activeExpenses = $('.expense').not('.hidden');
+    var total = 0;
+    $.each(activeExpenses, function(activeExpense) {
+        var amount = $(this).children().children('.amount')[0].innerText;
+        amount = parseInt(amount.slice(1));
+        total = total + amount;
+    });
+    $("#total").html(total);
+}
+
+$(document).ready(function(){
+    get_total();    
 });
